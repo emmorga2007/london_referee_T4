0e09ef3df16d726259f1d6111decd53e0b28534b
Gavin Brown <55294040+gavinbrown025@users.noreply.github.com>
2021-02-19 20:29:46 -0500

    footer styled

--- includes/footer.php
+++ includes/footer.php
@@ -1,30 +1,57 @@
 <footer class="footer">
-    <nav>
-        <ul>
-            <li>
+    <div class="footer-left">
+        <p>London Referee Group<br>
+        123 Hockey Ave<br>
+        London, On<br>
+        N6G 5J9</p>
+        <div class="socials">
+            <img src="images/facebook.svg" alt="">
+            <img src="images/instagram.svg" alt="">
+            <img src="images/twitter.svg" alt="">
+        </div>
+    </div>
+
+    <ul>
+        <li>
             <a href="<?php echo ROOT_PATH .'/index.php'?>">Home</a>
-            </li>
-            <li>
+        </li>
+        <li>
             <a href="<?php echo ROOT_PATH .'/about.php'?>">About</a>
-            </li>
-            <li>
+        </li>
+        <li>
             <a href="<?php echo ROOT_PATH .'/programs.php'?>">Programs</a>
-            </li>
-            <li>
+        </li>
+    </ul>
+
+    <ul>
+        <li>
             <a href="<?php echo ROOT_PATH .'/events.php'?>">Events</a>
-            </li>
-            <li>
+        </li>
+        <li>
             <a href="<?php echo ROOT_PATH .'/contact.php'?>">Contact</a>
-            </li>
-            <li>
+        </li>
+        <li>
             <a href="<?php echo ROOT_PATH .'/contact.php'?>">Apply Now</a>
-            </li>
-        </ul>
-        <ul>
-            <li>
+        </li>
+    </ul>
+
+    <ul>
+        <li>
+            <a href="<?php echo ROOT_PATH .'/events.php'?>">Events</a>
+        </li>
+        <li>
+            <a href="<?php echo ROOT_PATH .'/contact.php'?>">Contact</a>
+        </li>
+        <li>
+            <a href="<?php echo ROOT_PATH .'/contact.php'?>">Apply Now</a>
+        </li>
+    </ul>
+
+    <ul>
+        <img src="images/logo.svg" alt="">
+        <li>
             <a href="<?php echo ROOT_PATH .'/admin/admin_login.php'?>">Admin Login</a>
-            </li>
-        </ul>
-    </nav>
-    <p>Copyright LRG © <?php echo date('Y');?></p>
+        </li>
+    </ul>
 </footer>
+<p class="copyright">Copyright LRG © <?php echo date('Y');?></p>
